9d211b2b3872414bd7f6d4fb4300a9f3b637c17e
Poda-hup <abamin253@gmail.com>
2026-03-18 05:55:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let menu = document .querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclik = () =>{
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active')
+}
+const typed = new Typed('.multiple-text', {
+    strings: ['UI UX Designer','Web Designer','Frontend Developer','Data Analysis'],
+    typeSpeed: 80,
+    backSpedd:80,
+    backDelay:1200,
+    loop:true,
+  });
